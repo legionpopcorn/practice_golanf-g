8b31a4639692b4d6aedce453d456738fe4e50764
legionpopcorn <surajpariyar@Surajs-MacBook-Pro.local>
2021-03-16 10:00:58 -0700

    updates

--- go_tutorial/hello_test.go
+++ go_tutorial/hello_test.go
@@ -1,12 +1,26 @@
 package main
 
-// Add my new test cases for Golang code to print Hello World!
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("World")
-	want := "Hello, World"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
